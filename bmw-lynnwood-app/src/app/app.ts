abbc957998eda8b30ba93130eccd4f13beb4ed41
Justin Shaw <justin.avery.shaw@gmail.com>
2026-02-04 18:07:47 -0800

    API Integration

--- bmw-lynnwood-app/src/app/app.ts
+++ bmw-lynnwood-app/src/app/app.ts
@@ -1,14 +1,34 @@
 import { Component, signal } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { APIService } from './api.service';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+
+  private isLoading = true;
+
+  constructor(private api: APIService) {}
+
   protected readonly title = signal('bmw-lynnwood-app');
+
+  public async getFilterMenu() {
+    let menuData;
+    try {
+      menuData = await firstValueFrom(this.api.getMenuList());
+      this.isLoading = false;
+    
+    } catch (error) {
+      this.isLoading = false;
+    }
+    console.debug('menu data: ', menuData);
+  }
 
   
 }
